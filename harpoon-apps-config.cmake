5803ec97beb3f860acb9050fad85d8def677e64a
Sim Khai Lim <slim@presonus.com>
2024-08-07 11:59:34 +0000

    MPU-202 cmake format

--- harpoon-apps-config.cmake
+++ harpoon-apps-config.cmake
@@ -1,71 +1,51 @@
+# Copyright 2024 Fender Musical Instruments Corporation
+#
+# This file contains sensitive information that is privileged, confidential
+# and/or exempt from disclosure under applicable law. This file is the
+# PROPRIETARY property of the Fender Musical Instruments Corporation.
+
 set(harpoon_dir "${CMAKE_CURRENT_LIST_DIR}")
 
 add_library(harpoon-apps STATIC)
 
-target_compile_definitions(harpoon-apps PRIVATE
-    OS_ZEPHYR=1
-)
+target_compile_definitions(harpoon-apps PRIVATE OS_ZEPHYR=1)
 
-if(${TARGET_PEP_BOARD} STREQUAL "mimx8mn_evk_a53_smp")
-    target_compile_definitions(harpoon-apps PRIVATE
-        CONFIG_BOARD_MIMX8MN_EVK_A53=1
-    )
+if(${BOARD} STREQUAL "mimx8mn_evk_a53_smp")
+  target_compile_definitions(harpoon-apps
+                             PRIVATE CONFIG_BOARD_MIMX8MN_EVK_A53=1)
 endif()
 
-# Always add these sources
-target_sources(harpoon-apps PRIVATE
-    ${harpoon_dir}/common/libs/mailbox/mailbox.c
-)
+target_sources(harpoon-apps
+               PRIVATE ${harpoon_dir}/common/libs/mailbox/mailbox.c)
 
-target_include_directories(harpoon-apps PRIVATE
-	${harpoon_dir}
-	${harpoon_dir}/common/
-)
+target_include_directories(harpoon-apps PRIVATE ${harpoon_dir}
+                                                ${harpoon_dir}/common/)
 
 if(DEFINED ZEPHYR_BASE)
-    target_sources(harpoon-apps PRIVATE
-        ${harpoon_dir}/common/libs/jailhouse/ivshmem.c
-        ${harpoon_dir}/common/libs/hlog/hlog.c
-    )
+  target_sources(
+    harpoon-apps PRIVATE ${harpoon_dir}/common/libs/jailhouse/ivshmem.c
+                         ${harpoon_dir}/common/libs/hlog/hlog.c)
 
-    target_compile_options(harpoon-apps PRIVATE
-        -Wno-unused-variable
-        -Wno-sign-compare
-        -Wno-unused-parameter
-    )
+  target_compile_options(
+    harpoon-apps PRIVATE -Wno-unused-variable -Wno-sign-compare
+                         -Wno-unused-parameter)
 
-    target_include_directories(harpoon-apps PRIVATE
-        ${harpoon_dir}/common/libs/jailhouse/
-        ${harpoon_dir}/common/libs/hlog
-		${harpoon_dir}/common/zephyr
-		${harpoon_dir}/common/zephyr/boards
-		${ZEPHYR_BASE}/include
-    )
+  target_include_directories(
+    harpoon-apps
+    PRIVATE ${harpoon_dir}/common/libs/jailhouse/
+            ${harpoon_dir}/common/libs/hlog ${harpoon_dir}/common/zephyr
+            ${harpoon_dir}/common/zephyr/boards ${ZEPHYR_BASE}/include)
 
-	target_link_libraries(harpoon-apps PRIVATE kernel)
+  target_link_libraries(harpoon-apps PRIVATE kernel)
 
 elseif(DEFINED LINUX)
-    ################################
-    # This is a linux build
-    ################################
+  target_sources(harpoon-apps PRIVATE ${harpoon_dir}/ctrl/ivshmem.c)
 
-    target_sources(harpoon-apps PRIVATE
-        ${harpoon_dir}/ctrl/ivshmem.c
-    )
+  target_include_directories(harpoon-apps PRIVATE ${harpoon_dir}/ctrl)
 
-    target_include_directories(harpoon-apps PRIVATE
-        ${harpoon_dir}/ctrl
-    )
+  target_include_directories(harpoon-apps PUBLIC ${harpoon_dir})
 
-    target_include_directories(harpoon-apps PUBLIC
-        ${harpoon_dir}
-    )
-
-    target_include_directories(pep PUBLIC ${harpoon_dir})
+  target_include_directories(pep PUBLIC ${harpoon_dir})
 else()
-	message(FATAL_ERROR "No supported Platform detected")
+  message(FATAL_ERROR "No supported Platform detected")
 endif()
-
-
-
-
